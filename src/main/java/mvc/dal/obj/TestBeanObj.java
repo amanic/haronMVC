b1301dccfc6d66389482247822c6118da451873a
martea <1642485710@qq.com>
2018-08-24 17:42:39 +0800

    测试beanfactory.getbean

---
+++ src/main/java/mvc/dal/obj/TestBeanObj.java
@@ -0,0 +1,16 @@
+package mvc.dal.obj;
+
+import lombok.Data;
+
+/**
+ * Created by martea on 2018/8/24.
+ */
+@Data
+public class TestBeanObj {
+
+    private String string;
+
+    private Integer integer;
+
+    private Object object;
+}
--- src/test/beanFactoryTest/BeanFactoryTest.java
+++ src/test/beanFactoryTest/BeanFactoryTest.java
@@ -1,9 +1,13 @@
 package beanFactoryTest;
 
+import mvc.dal.obj.TestBeanObj;
 import mvc.service.TestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,8 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by martea on 2018/8/24.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:META-INF/spring/applicationContext.xml",
-        "classpath*:META-INF/spring/dispatcher-servlet.xml"})
+@ContextConfiguration(locations = {"classpath*:META-INF/spring/applicationContext.xml"})
 public class BeanFactoryTest {
 
     @Autowired
@@ -21,5 +24,12 @@ public class BeanFactoryTest {
     @Test
     public void testBeanFactoryLoad(){
         System.out.println(testService.testString("hello world!"));
+    }
+
+    @Test
+    public void testGetBeanFromXml(){
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("META-INF/spring/applicationContext.xml"));
+        TestBeanObj obj = (TestBeanObj) beanFactory.getBean("beanObj");
+        System.out.println(obj.toString());
     }
 }
