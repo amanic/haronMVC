9359925b260292fb0f17cd5331c6f04efd73a737
martea <1642485710@qq.com>
2018-08-29 16:58:11 +0800

    测试@InitBinder

--- src/main/java/mvc/controller/TestController.java
+++ src/main/java/mvc/controller/TestController.java
@@ -3,13 +3,18 @@ package mvc.controller;
 import mvc.objects.ResultApi;
 import mvc.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by martea on 2018/8/23.
@@ -21,6 +26,13 @@ public class TestController {
 
     @Autowired
     TestService testService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) throws Exception {
+        DateFormat df = new SimpleDateFormat("yyyy---MM---dd HH:mm:ss");
+        CustomDateEditor dateEditor = new CustomDateEditor(df, true);
+        binder.registerCustomEditor(Date.class, dateEditor);
+    }
 
 
     @RequestMapping(value = "test1")
@@ -69,5 +81,21 @@ public class TestController {
     @ResponseBody
     public String test7(){
         return testService.testString4("");
+    }
+
+    /**
+     * 测试{@link InitBinder}
+     * @param date
+     * @return
+     */
+    @RequestMapping(value="test8",method= RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> test8(Date date){
+        Map<String,Object> map=new HashMap<String,Object>();
+        map.put("name","lg");
+        map.put("age",23);
+        map.put("date",new Date());
+        map.put("old",date);
+        return map;
     }
 }
